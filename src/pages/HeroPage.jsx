2e5b8f8e0c7e34d2f30eb948d3f0393e20f5bc2f
Believe Kwamitse <believeserwaa80@gmail.com>
2025-03-15 11:02:27 +0000

    3rd

--- src/pages/HeroPage.jsx
+++ src/pages/HeroPage.jsx
@@ -28,20 +28,29 @@ const HeroPage = () => {
         </p>
       </motion.div>
 
-      {/* Image */}
+      {/* Image with bounce animation */}
       <motion.div
-        className="mt-6 md:mt-0 md:w-1/2 flex justify-center md:justify-end lg:justify-end pr-6 md:pr-12"
+         className="mt-3 sm:mt-4 md:mt-0 md:w-1/2 flex justify-center md:justify-end lg:justify-end pr-6 md:pr-12"
+
         initial={{ opacity: 0, x: 50 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 1, ease: "easeOut" }}
       >
-        <div>
+        <motion.div
+          animate={{ y: [0, -15, 0] }} // Bouncing effect
+          transition={{
+            repeat: Infinity,
+            repeatType: "reverse",
+            duration: 1.5,
+            ease: "easeInOut",
+          }}
+        >
           <img
             src="/bee.jpg"
             alt="Hero Image"
-            className="rounded-full w-64 h-64 sm:w-80 sm:h-80 md:w-96 md:h-96 object-cover shadow-2xl border-6 border-blue-400 hover:border-blue-300 transition-all duration-300"
+            className="rounded-full w-72 h-72 sm:w-96 sm:h-96 md:w-[28rem] md:h-[28rem] object-cover shadow-2xl border-6 border-blue-400 hover:border-blue-300 transition-all duration-300"
           />
-        </div>
+        </motion.div>
       </motion.div>
     </section>
   );
